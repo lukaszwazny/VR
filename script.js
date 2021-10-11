02443aac1e8e0b9aefa10d82111e3f8ecde32e86
lukaszwazny <32484689+lukaszwazny@users.noreply.github.com>
2021-10-11 21:49:39 +0200

    fix

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let box = document.querySelector("a-box")
-let boxRot = box.getAttribute('rotation')
 
-while(true){
-    boxRot.x += 1
-    boxRot.y += 1
-    boxRot.z += 1
-    document.querySelector("a-box").setAttribute('rotation', boxRot)
-    await sleep(100)
+
+window.onload = function () {
+
+    let box = document.querySelector("a-box")
+    let boxRot = box.getAttribute('rotation')
+
+    while(true){
+        boxRot.x += 1
+        boxRot.y += 1
+        boxRot.z += 1
+        document.querySelector("a-box").setAttribute('rotation', boxRot)
+        await sleep(100)
+    }
+
 }
-
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
